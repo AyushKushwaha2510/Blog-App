65f90a0732fd2d0b499ce397c90670ed11da2902
AyushKushwaha2510 <ayush.kushwaha23138@gmail.com>
2025-06-30 22:36:20 +0530

    click to read

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import service from '../appwrite/config'
 
-function PostCard({$id, title, featuredImage}) {
-    
+function PostCard({ $id, title, featuredImage }) {
+
   return (
     <Link to={`/post/${$id}`}>
-        <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                <img src={service.getFilePreview(featuredImage)} alt={title}
-                className='rounded-xl' />
+      <div className='w-full bg-gray-100 rounded-xl p-4'>
+        <div className='w-full justify-center mb-4'>
+          <img src={service.getFilePreview(featuredImage)} alt={title}
+            className='rounded-xl' />
 
-            </div>
-            <h2
-            className='text-xl font-bold'
-            >{title}</h2>
         </div>
+        <h2
+          className='text-xl font-bold mb-5'
+        >{title}</h2>
+        <span className='text-[16px] mt-5'>Click to Read</span>
+      </div>
+
     </Link>
   )
 }
