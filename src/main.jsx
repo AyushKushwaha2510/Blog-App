109bc0e9aa75d49cdd31f096727b27dc3c9a9b71
ayushk2024ug1020-ux <ayushk.2024ug1020@iiitranchi.ac.in>
2026-02-01 16:42:22 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,6 @@ import EditPost from "./pages/EditPost";
 import Post from "./pages/Post";
 import OAuthHandler from "./components/OAuthHandler";
 
-
 const router = createBrowserRouter([
   {
     path: '/',
